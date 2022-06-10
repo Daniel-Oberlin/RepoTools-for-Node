0ece8a51b31a14d754ace6b140f83ead4a46b03f
Daniel Oberlin <dmo.git@gmail.com>
2022-06-10 11:44:30 -0400

    Figure out how to use yargs

--- repotool.js
+++ repotool.js
@@ -3,3 +3,18 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
+const args = yargs(hideBin(process.argv))
+  .command('status', 'check the status of the repo')
+  .option('detail', {
+    alias: 'd',
+    type: 'boolean',
+    default: false,
+    description: "Preview changes but don't make them"
+  })
+  .strict()
+  .demandCommand(1, 1, 'You need to specify a command')
+  .parse();
+
+const command = args._[0];
+
+  console.log();
