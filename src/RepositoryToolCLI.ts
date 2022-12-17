6cf3aed016dc47e57bab00f76dbf647066d0e5f2
Daniel Oberlin <dmo.git@gmail.com>
2022-12-17 10:24:47 -0500

    Track moves and track duplicates

--- src/RepositoryToolCLI.ts
+++ src/RepositoryToolCLI.ts
@@ -22,6 +22,8 @@ export default class RepositoryToolCLI {
         this.command == 'validate' && (this.tool.alwaysCheckHash = true);
         this.args.showProgress && (this.tool.showProgress = true);
         this.args.trackMoves && (this.tool.trackMoves = true);
+        this.args.trackDuplicates && (this.tool.trackDuplicates = true);
+
     }
 
     public async run() { await this.tool.update(); }
@@ -34,8 +36,13 @@ export default class RepositoryToolCLI {
         different = this.reportFiles(this.tool.changedFiles, 'have changed content') || different;
         different = this.reportFiles(this.tool.newFiles, 'are new') || different;
         different = (this.reportFiles(this.tool.lastModifiedDateFiles, 'have last-modified dates which are different') && !this.args.ignoreDate) || different;
+        different = this.reportFiles(this.tool.errorFiles, 'have errors') || different;
         different = this.reportMovedFiles(this.tool.movedFileOrder, this.tool.movedFiles) || different;
 
+        this.reportDuplicateFiles(this.tool.duplicateFiles);
+        this.reportFiles(this.tool.newlyIgnoredFiles, 'are newly ignored');
+        this.reportFiles(this.tool.ignoredFiles, 'were ignored', 1);
+        
         return different;
     }
 
@@ -61,14 +68,14 @@ export default class RepositoryToolCLI {
 
     }
 
-    protected reportFiles(files: ManifestFile[], description:string ): boolean {
+    protected reportFiles(files: ManifestFile[], description:string, moreThan:number = 0): boolean {
 
-        if (files.length > 0) {
+        if (files.length > moreThan) {
             console.log(`${files.length} files ${description}.`);
 
             if (this.args.detail) {
-            files.map( manFile => console.log(`   ${Manifest.makeStandardFilePathString(manFile)}`));
-            console.log('');
+                files.map( manFile => console.log(`   ${Manifest.makeStandardFilePathString(manFile)}`));
+                console.log();
             }
 
             return true;
@@ -89,14 +96,14 @@ export default class RepositoryToolCLI {
 
                     let files = movedFiles.get(movedFileKey);
                     if (files != undefined) {
-                    let out = '   ';
-                    let [oldFiles, newFiles] = files;
+                        let out = '   ';
+                        let [oldFiles, newFiles] = files;
 
-                    oldFiles.map(oldFile => out += `${Manifest.makeStandardFilePathString(oldFile)} `);
-                    out += '->';
-                    newFiles.map(newFile => out += ` ${Manifest.makeStandardFilePathString(newFile)}`);
+                        oldFiles.map(oldFile => out += `${Manifest.makeStandardFilePathString(oldFile)} `);
+                        out += '->';
+                        newFiles.map(newFile => out += ` ${Manifest.makeStandardFilePathString(newFile)}`);
 
-                    console.log(out);
+                        console.log(out);
                     }
 
                 });
@@ -109,6 +116,25 @@ export default class RepositoryToolCLI {
 
         return false;
     }
+
+    protected reportDuplicateFiles(duplicateFiles: Map<string, ManifestFile[]>) {
+
+        if (duplicateFiles.size > 0) {
+            console.log(`${duplicateFiles.size} file hashes were duplicates.`);
+
+            if (this.args.detail) {
+
+                for (let [hash, files] of duplicateFiles) {
+                    console.log(`   Hash: ${hash}`);
+                    files.map(file => console.log(`      ${Manifest.makeStandardFilePathString(file)}`));
+                }
+
+            }
+                
+            console.log();
+        }
+
+    }
     
     // Process the command line arguments here because I don't know how to
     // give a concrete type to this member without explicitly initializing
@@ -117,24 +143,28 @@ export default class RepositoryToolCLI {
         .command('status', 'Check every file in the manifest based on file length and last modified date.  Check the hash values of modified files.  Report new files, missing files, and any differences.')
         .command('validate', 'Check every file in the manifest against its hash value.  Report newfiles, missing files, and any differences.')
         .option('detail', {
-          alias: 'd',
-          type: 'boolean',
-          default: false,
-          description: 'List the actual files that are different - not just the counts.'})
+            alias: 'd',
+            type: 'boolean',
+            default: false,
+            description: 'List the actual files that are different - not just the counts.'})
         .option('showProgress', {
-          alias: 'p',
-          type: 'boolean',
-          default: false,
-          description: 'List each file as it is being scanned.'})
+            alias: 'p',
+            type: 'boolean',
+            default: false,
+            description: 'List each file as it is being scanned.'})
         .option('trackMoves', {
-          alias: 'm',
-          type: 'boolean',
-          default: false,
-          description: 'Try to identify files that have been renamed or moved based on their hash values.'})
+            alias: 'm',
+            type: 'boolean',
+            default: false,
+            description: 'Try to identify files that have been renamed or moved based on their hash values.'})
+        .option('trackDuplicates', {
+            type: 'boolean',
+            default: false,
+            description: 'Try to identify duplicate files based on their hash values.'})
         .option('ignoreDate', {
-          type: 'boolean',
-          default: false,
-          description: "Don't count a date change alone as being a significant difference between the manifest and the repository.  Don't return an error exit code for this difference alone.  Useful for status and validate commands.."
+            type: 'boolean',
+            default: false,
+            description: "Don't count a date change alone as being a significant difference between the manifest and the repository.  Don't return an error exit code for this difference alone.  Useful for status and validate commands.."
         })
         .strict()
         .demandCommand(1, 1, 'You need to specify a command')
